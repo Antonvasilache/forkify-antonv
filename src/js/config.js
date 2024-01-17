e15b40f9b19078a426928e274590d0e2faec75ae
Varthag <vasilacheanton91@gmail.com>
2024-01-17 16:31:37 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const FIRST_RES_PAGE = 1;
+export const KEY = `81282415-a6c0-480b-9212-2d878d79889a`;
+export const MODAL_CLOSE_SECONDS = 2.5;
